8266d340ebe93b46df6da8c84f011bd4efa16b6e
Hanul <hanul@hanul.me>
2021-10-27 16:27:33 +0900

    fix vers

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ import PolygonWallet from "./polygon/PolygonWallet";
     let selectedId: number | undefined;
 
     let disk: DomNode;
+    let bar: DomNode;
     let walletAddress;
 
     BodyNode.append(el(".layout",
@@ -16,17 +17,26 @@ import PolygonWallet from "./polygon/PolygonWallet";
             new Item(0, "JADE KEY ver.", "#A93DA2", () => {
                 disk.empty().append(el("img", { src: "/images/LP1.png" }));
                 selectedId = 0;
+                bar.style({
+                    left: 996,
+                    top: 35,
+                    transform: "rotate(30deg)",
+                });
             }),
-            new Item(1, "Graynym ver.", "#4D61F0", () => {
+            new Item(1, "beat.k ver.", "#4D61F0", () => {
                 disk.empty().append(el("img", { src: "/images/LP2.png" }));
                 selectedId = 1;
             }),
-            new Item(2, "beat.k ver.", "#FCFC0B", () => {
+            new Item(2, "Graynym ver.", "#FCFC0B", () => {
                 disk.empty().append(el("img", { src: "/images/LP3.png" }));
                 selectedId = 2;
             }),
         ),
         disk = el(".disk"),
+        bar = el(".bar-container",
+            el("img.bar", { src: "/images/bar.png", height: "400" }),
+            el("img.slot", { src: "/images/slot.png", height: "70" }),
+        ),
         el(".mint-form",
             el("h4", "Wallet Address"),
             walletAddress = el(".wallet-address"),
